54ef7ec7697ff78977bd8c5afce2b0f72e352de6
Varun_RR <varun.rayabarapu@healthipass.com>
2024-08-27 21:59:09 -0700

    Separate app starter and response generator logic

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from openai import OpenAI
 import streamlit as st
 
+from response_generator import response_generator
+
 st.title("ChatGPT-like clone")
 
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+    st.session_state["openai_model"] = "gpt-4o-mini"
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -17,18 +19,11 @@ for message in st.session_state.messages:
 
 if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
+
     with st.chat_message("user"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-        response = st.write_stream(stream)
-        print(response)
+        response = response_generator(st, client)
+
     st.session_state.messages.append({"role": "assistant", "content": response})
---
+++ response_generator.py
@@ -0,0 +1,10 @@
+def response_generator(st, client):
+    stream = client.chat.completions.create(
+        model=st.session_state["openai_model"],
+        messages=[
+            {"role": m["role"], "content": m["content"]}
+            for m in st.session_state.messages
+        ],
+        stream=True,
+    )
+    return st.write_stream(stream)
